305a19053a90553cdbb09b2f9229ec5308892070
Sraavan Chevireddy <schevireddy@saventech.com>
2021-06-27 19:22:50 +0530

    Docstrings added

--- TrailTwo.py
+++ TrailTwo.py
@@ -1,48 +1,65 @@
+"""
+This is the Vertext class that hold the vertices and adjacent neighbours as edges. 
+The list of edges contain a start vertex and end vertex, through which the code indices match for airport algo.
+"""
 class Vertex:
     def __init__(self, n):
         self.name = n
         self.edges = list()
 
-
+"""
+Edges contains the start vetext and end vertex joining the indices. Start and end are the neighbours that will be linked via Graph edge list.
+"""
 class Edges:
     def __init__(self, vertex):
         self.destinationVertex = vertex
 
-
+"""
+To read the input from the input.txt file and render the values that are next to '=' symbol.
+returns a String of airport value that is added next to 'StatingAirport'.
+"""
 def read_input(fromPath):
     with open(fromPath) as f:
         lines = f.readlines()[0]
         return lines.rsplit('=', 1)[1]
 
-
+"""
+Graph edge list will have the edges and vertices stored in different arrays.
+They are mapped together with edge list. 
+"""
 class Graph:
 
+    """Initilazing a empty list of vertices on initializing graph"""
     def __init__(self):
         self.vertices = list()
 
+    """Adding vertices to the list of initialzed graph. """
     def add_vertex(self, vertex):
         self.vertices.append(vertex)
 
+    """
+    Takes vertex one and vertext two as parameters and adds the vertices to the actual pointer location.
+    The graph is only one directional. So only vertext one is added to vertex two but not vice versa.
+    """
     def add_edges(self, vertex_one, vertex_two):
         return vertex_one.edges.append(Edges(vertex_two))  # Since graph is one directional
 
+    """
+    Taking the input airport name from input.text as parameter this program filters the adjacent airports.
+    And the next adjacent airport is again filtered with the corresponding neighbour respectively. 
+    If no airport is found for edges is written back to the 'output.txt' file
+    """
     def print_graph(self, input_airport):
         input_airport_vertex = list(filter(lambda vertex: vertex.name == input_airport, self.vertices))[0]
         print(f'Airport Name is {input_airport_vertex.name}')
         array_of_no_connections = list()
         for each in self.vertices:
-            if each.edges:
-                array_of_no_connections.append(each.name)
+            if each.edges: #Checking if the edge is empty for input airport
+                array_of_no_connections.append(each.name) # Appending the missing connection from input airport.
             else: print(f'{each.name} has no connections')
-        f= open("output.txt","w+")
-        print(f'Minimum Number of flights that needs to be added are {len(array_of_no_connections)}')
-        print(f'Flights that need to be added are')
-        for arr in array_of_no_connections:
-            print(f'{input_airport_vertex.name}, {arr}')
-        f.write(f'Minimum Number of Flights that needs to be added are {len(array_of_no_connections)}\n')
-        f.readline()
+        f= open("outpu56t.txt","w+")
+        f.write(f'Minimum Number of Flights that needs to be added are {len(array_of_no_connections)}\n') 
         f.write('Flights that need to be added are\n')
-        f.readline()
         for arr in array_of_no_connections:
             f.write(f'{input_airport_vertex.name}, {arr}\n')
             f.readline()
@@ -50,11 +67,13 @@ class Graph:
 
 
 
-inputFlight = read_input('./input.txt')
+inputFlight = read_input('./input56.txt')
 
+# List of Airports given in problem bank
 list_of_airports = {'BGI', 'CDG', 'DEL', 'DOH', 'DSM', 'EWR', 'EYW', 'HND', 'ICN', 'JFK', 'LGA', 'LHR', 'ORD', 'SAN',
                     'SFO', 'SIN', 'TLV', 'BUD'}
 
+# Edges that are given in the Problem Bank
 edges = [
     "DSM,ORD",
     "ORD,BGI",
